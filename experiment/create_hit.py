e920a9da1019725d6917361bbdd1fb472f8046a6
Jonathan Glidden <jhglidden@gmail.com>
2014-05-01 09:28:24 -0700

    update hit creation script

--- experiment/create_hit.py
+++ experiment/create_hit.py
@@ -12,28 +12,9 @@ mtc = MTurkConnection(
         host=HOST)
 
 
-#
-#overview = Overview()
-#overview.append_field('Title', 'Psychology Experiment')
-#overview.append(FormattedContent('<a target="_blank"'
-#                                 ' href="http://murmuring-inlet-9267.herokuapp.com/">'
-#                                 ' Click me</a>'))
-#
-#question_content = QuestionContent()
-#question_content.append_field('Title','Your personal comments')
-# 
-#fta = FreeTextAnswer()
-# 
-#question = Question(identifier="comments",
-#              content=question_content,
-#              answer_spec=AnswerSpecification(fta))
-#
-#question_form = QuestionForm()
-#question_form.append(overview)
-#question_form.append(question)
-question_form = ExternalQuestion(external_url='https://cocosci.berkeley.edu:8000/', frame_height=800)
+question_form = ExternalQuestion(external_url='https://fruitexp.dreamhosters.com', frame_height=800)
 keywords=['boto', 'test', 'doctest']
-create_hit_rs = mtc.create_hit(question=question_form, max_assignments=10,title="Categorization Experiment 2", keywords=keywords,reward = 0.05, duration=60*6,approval_delay=60*60)
+create_hit_rs = mtc.create_hit(question=question_form, max_assignments=10,title="Categorization Experiment 3", keywords=keywords,reward = 0.05, duration=60*6,approval_delay=60*60)
 assert(create_hit_rs.status == True)
 
 
