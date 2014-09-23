c6bc9f9848e522d1cd7ba430e46f0be5c4470ff4
Jonathan Glidden <jhglidden@gmail.com>
2014-09-23 15:57:51 -0700

    add function for updating qualifications

--- experiment/create_hit.py
+++ experiment/create_hit.py
@@ -16,29 +16,40 @@ mtc = MTurkConnection(
 ASSIGNMENTS = 2
 DAYS = 5
 QUALIFICATION_ID = '3SLCM7XLWH8WO61VD7YZPK4Q3U9T69'
-for day in range(DAYS):
-    requirements = []
-    if day != 0:
-        r = Requirement(QUALIFICATION_ID, 'EqualTo', day)
-        requirements.append(r)
-    worker_percent_assignments_approved = Requirement("000000000000000000L0", 'EqualTo', 95)
-    requirements.append(worker_percent_assignments_approved)
+def main():
+    for day in range(DAYS):
+        requirements = []
+        if day != 0:
+            r = Requirement(QUALIFICATION_ID, 'EqualTo', day)
+            requirements.append(r)
+        worker_percent_assignments_approved = Requirement("000000000000000000L0", 'EqualTo', 95)
+        requirements.append(worker_percent_assignments_approved)
 
-    title = 'Hierarchical Categorization Learning {0}'.format(day)
-    question_form = ExternalQuestion(external_url='https://fruitexp.dreamhosters.com', frame_height=1000)
-    keywords=['psychology', 'experiment', 'categorization', 'learning', 'pictures']
-    description='Learn a hierchary of categories by iterated guessing'
-    create_hit_rs = mtc.create_hit(
-            question=question_form,
-            max_assignments=2,
-            title=title,
-            keywords=keywords,
-            description=description,
-            reward=3.00+day*.50,
-            duration=2*60*60,
-            lifetime=5*24*60*60,
-            approval_delay=7*24*60*60,
-            qualifications=Qualifications(requirements))
-    assert(create_hit_rs.status == True)
+        title = 'Hierarchical Categorization Learning {0}'.format(day)
+        question_form = ExternalQuestion(external_url='https://fruitexp.dreamhosters.com', frame_height=1000)
+        keywords=['psychology', 'experiment', 'categorization', 'learning', 'pictures']
+        description='Learn a hierchary of categories by iterated guessing'
+        create_hit_rs = mtc.create_hit(
+                question=question_form,
+                max_assignments=2,
+                title=title,
+                keywords=keywords,
+                description=description,
+                reward=3.00+day*.50,
+                duration=2*60*60,
+                lifetime=5*24*60*60,
+                approval_delay=7*24*60*60,
+                qualifications=Qualifications(requirements))
+        assert(create_hit_rs.status == True)
+
+def update_qualification(worker_id, value):
+    if value == 1:
+        mtc.assign_qualification(QUALIFICATION_ID, worker_id, value)
+    else:
+        current_score = int(mtc.get_qualification_score(QUALIFICATION_ID, worker_id)[0].IntegerValue)
+        assert value == (current_score + 1)
+        mtc.update_qualification_score(QUALIFICATION_ID, worker_id, value)
 
 
+if __name__ == '__main__':
+    main()
